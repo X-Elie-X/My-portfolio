7b51e716355b18b50564c3508c72d9160b26b397
AduwuJoseph <aduwujoseph4real@gmail.com>
2022-07-18 16:23:43 +0100

    Added the close button the mobile menu and style the display

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 let mobileMenu = () => {
-  alert();
   let x = document.getElementById("listinfo");
+  let icon = document.getElementById("menu-icon");
   if (x.style.display === "block") {
+    icon.innerHTML = '<i class="fa fa-bars 2x"></i>'; 
     x.style.display = "none";
   } else {
+    icon.innerHTML = '<i class="fa fa-times-circle"></i>'; 
     x.style.display = "block";
   }
 };
