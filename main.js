a0f1892a3e3c7777dc840680013719c37b534571
X-Elie-X <103565978+X-Elie-X@users.noreply.github.com>
2022-07-18 16:11:43 +0200

    Update readme and install linters.js

---
+++ main.js
@@ -0,0 +1,8 @@
+let mobileMenu = () => {
+  let x = document.getElementById("listinfo");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+};
