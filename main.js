cd739941b66dbc00c8436582937965987ab55100
X-Elie-X <103565978+X-Elie-X@users.noreply.github.com>
2022-07-18 17:53:04 +0200

    fix linter errors

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-const mobileMenu = () => {
-  const x = document.getElementById('listinfo');
-  const icon = document.getElementById('menu-icon');
-  if (x.style.display === 'block') {
-    icon.innerHTML = '<i class="fa fa-bars 2x"></i>'; 
-    x.style.display = 'none';
+function mobileMenu() {
+  const x = document.getElementById("listinfo");
+  const icon = document.getElementById("menu-icon");
+  if (x.style.display === "block") {
+    icon.innerHTML = '<i class="fa fa-bars 2x"></i>';
+    x.style.display = "none";
   } else {
-    icon.innerHTML = '<i class="fa fa-times-circle"></i>'; 
-    x.style.display = 'block';
+    icon.innerHTML = '<i class="fa fa-times-circle"></i>';
+    x.style.display = "block";
   }
-};
+}
