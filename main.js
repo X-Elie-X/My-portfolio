17e99b0d6e2f024a943d6870bede7dc46218a402
X-Elie-X <103565978+X-Elie-X@users.noreply.github.com>
2022-07-20 08:55:23 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,36 @@
+const listInfo = document.getElementById('listinfo');
+const icon = document.getElementById('menu-icon');
+const btn = document.querySelectorAll('#btn');
+const popup = document.getElementById('popup1');
+const close = document.querySelector('#close-popup');
 function mobileMenu() {
-  const x = document.getElementById("listinfo");
-  const icon = document.getElementById("menu-icon");
-  if (x.style.display === "block") {
+  if (listInfo.style.display === 'block') {
     icon.innerHTML = '<i class="fa fa-bars 2x"></i>';
-    x.style.display = "none";
+    listInfo.style.display = 'none';
   } else {
     icon.innerHTML = '<i class="fa fa-times-circle"></i>';
-    x.style.display = "block";
+    listInfo.style.display = 'block';
   }
 }
-const element = document.getElementById("menu-icon");
-element.addEventListener("click", mobileMenu);
+const element = document.getElementById('menu-icon');
+element.addEventListener('click', mobileMenu);
+
+function openPopup() {
+  popup.style.display = 'block';
+}
+btn.forEach((btn) => {
+  btn.addEventListener('click', openPopup);
+});
+
+function closePopup() {
+  popup.style.display = 'none';
+}
+close.addEventListener('click', closePopup);
+
+// function on() {
+//   document.getElementById('#overlay').style.display = 'bloack';
+// }
+
+// function off() {
+//   document.getElementById('#overlay').style.display = 'none';
+// }
